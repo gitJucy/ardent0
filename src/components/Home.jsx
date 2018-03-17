6c6665bc16c51764106ef132a612480bb4ba6a9c
gitJucy <johnrlucy84@gmail.com>
2018-03-17 00:40:06 -0400

    responsive design of homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,18 +4,18 @@ import React ,{Component} from 'react'
 class Home extends Component {
     render() {
         return (
-            <div className='main'>
-                <div className='landing'>
+            <div className='main container-fluid'>
+                <div className='landing container'>
                     <h1>ARDENT</h1>
                 </div>
-                <div className='about-box container'>
-                    <div className='row'>
-                        <div className=' col-md-6'id='abpic'></div> 
-                       
-                        <div className='wording-main container col-md-6'>
-                            <p>hfkjhksda asdhfk shdf sdfk hksjdhf kjsdhfjkshdfkjh sdfsdkjfhsdkh sdjhsdfjdsh fhjd dhjd djfs sdkfjhk sd sdfkdhfk sd  sdfkhkj dfj jdf jsdh fjshdjf ds</p>
-                        </div> 
-                     </div>
+                <div className='row justify-content-center about-box'>
+                    <div className='col-sm-5 abpic'>
+                    </div> 
+                    <div className='col-sm-1'></div>
+                    <div className='wording-main col-sm-5'>
+                    <p>Lorem ipsum dolor sit amet, vim et vero detraxit. Quod hinc ex nam, et sit error accumsan cotidieque, habeo noster no usu. Te reque vidisse definitionem vel. Has ea quidam option facilis, sit labore vidisse ex. Duo nemore antiopam inciderint ea. Justo ullum disputationi pro cu, nec vidit nonumy at.
+Quod integre pro no. Mea te vocent latine. Eos ea qualisque incorrupte contentiones, vel dicta graeco audire ad. Habeo malis petentium id pro. Ad mel nihil equidem admodum, quo te facilis philosophia, sumo dolor sanctus et qui. Cum ei saepe affert voluptatibus, vis ut lorem dictas ocurreret.</p>
+                    </div> 
                 </div>
             </div>
         );
