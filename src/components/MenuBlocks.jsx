09c187b87abc1cf075e133d6a554e31ad0747fc6
gitJucy <johnrlucy84@gmail.com>
2018-03-18 10:59:26 -0400

    navblock anchors added, fixed apple products bug

--- src/components/MenuBlocks.jsx
+++ src/components/MenuBlocks.jsx
@@ -4,38 +4,38 @@ class MenuBlock extends Component {
     render() {
         return (
             <div className='container-fluid MenuBlock-wrapper'>
-                <div className='row row1 d-flex justify-content-center align-items-center'>
+                <div className='row row1 d-flex justify-content-center align-content-center'>
                     <div className='col-sm'>
-                        <div className='box1'>
+                        <a href='#'><div className='box1'>
                             <h1>New Sales</h1>
-                        </div>
+                        </div></a>
                     </div>
                     <div className='col-sm'>
-                        <div className='box2'>
+                    <a href='#'><div className='box2'>
                             <h1>Used Sales</h1>
-                        </div>
+                        </div></a>
                     </div>
                     <div className='col-sm'>
-                        <div className='box3'>
+                    <a href='#'><div className='box3'>
                             <h1>Rentals</h1>
-                        </div>
+                        </div></a>
                     </div>
                 </div>
-                <div className='row row2 d-flex justify-content-center align-items-center'>
+                <div className='row row2 d-flex justify-content-center align-content-center'>
                     <div className='col-sm'>
-                        <div className='box4'>
+                    <a href='#'><div className='box4'>
                             <h1>Service</h1>
-                        </div>
+                        </div></a>
                     </div>
                     <div className='col-sm'>
-                        <div className='box5'>
+                    <a href='#'><div className='box5'>
                             <h1>Parts</h1>
-                        </div>
+                        </div></a>
                     </div>
                     <div className='col-sm'>
-                        <div className='box6'>
+                    <a href='#'><div className='box6'>
                             <h1>Contact</h1>
-                        </div>
+                        </div></a>
                     </div>
                 </div>
             </div>
