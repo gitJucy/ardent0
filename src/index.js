b81e3633f5b4f93042e4ba9cbf856fd7ac6a029a
gitJucy <johnrlucy84@gmail.com>
2018-04-14 09:52:15 -0400

    changes to render

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { render } from 'react-snapshot';
+// import { render } from 'react-snapshot';
+import ReactDOM from 'react-dom'
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
-render(<App />, document.getElementById('root'));
+ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
