4229ac78e9b8fe3b207e190cc8457e2df25374aa
gitJucy <johnrlucy84@gmail.com>
2018-04-23 16:58:50 -0400

    final touches before delivery

--- src/components/SalesImage.jsx
+++ src/components/SalesImage.jsx
@@ -9,24 +9,24 @@ import fork6 from'../img/fork6.png'
 import fork7 from'../img/fork7.png'
 import fork8 from'../img/fork8.png'
 import fork9 from'../img/fork9.png'
-import s25 from '../img/s25.pdf'
-import c25c from '../img/c25c.pdf'
-import cgc55 from '../img/cgc55.pdf'
-import cgc70 from '../img/cgc70.pdf'
-import c55 from '../img/c55.pdf'
-import c80 from '../img/c80.pdf'
+import s25 from '../img/s25.jpg'
+import s25p from '../img/s25p.jpg'
+import cgc55 from '../img/cgc55.jpg'
+import cgc70 from '../img/cgc70.jpg'
+import c55 from '../img/c55.jpg'
+import c80 from '../img/c80.jpg'
 
 class SalesImage extends Component{
     render(){
         return(
-            <div className='img-wrapper'>
-                <h1 style={{color:'white'}}><a href='https://www.clarkmhc.com'><img src={clark}/></a> Product Spotlight</h1><hr/>
+            <div id='sales-img-header'className='row img-wrapper'>
+                <h1 style={{color:'white'}}><a href='https://www.clarkmhc.com'><img src={clark}/></a> Product Spotlight</h1>
                 <div className ='row'>
             <div className='col col-sm-3 featured-img'></div>
             <div className='col-sm-9'>
                 <div className='row0 d-flex justify-content-center align-items-center'>
                     <a href={s25}target='_blank'><img className='col-sm-3 img0' src={fork8}/><p style={{color:'rgb(255,255,255)',textDecoration:'none'}}>S 20/25/30/35</p></a>
-                    <a href={c25c}target='_blank'><img className='col-sm-3 img0'src={fork0}/><p style={{color:'rgb(255,255,255)',textDecoration:'none'}}>S 20/25/30/35 IC-Pneumatic</p></a>
+                    <a href={s25p}target='_blank'><img className='col-sm-3 img0'src={fork0}/><p style={{color:'rgb(255,255,255)',textDecoration:'none'}}>S 20/25/30/35 IC-Pneumatic</p></a>
                     <a href={cgc55}target='_blank'><img className='col-sm-3 img0'src={fork2}/><p style={{color:'rgb(255,255,255)',textDecoration:'none'}}>CGC 40/50/55</p></a>
                 </div>
                 <div className='row1 d-flex justify-content-center align-items-center'>
