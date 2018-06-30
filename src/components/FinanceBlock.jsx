93d25994e5b310435ad5f9ff557b315eb44ac430
gitJucy <johnrlucy84@gmail.com>
2018-06-30 13:40:39 -0400

    last alt images and favicon fix

--- src/components/FinanceBlock.jsx
+++ src/components/FinanceBlock.jsx
@@ -7,7 +7,7 @@ class FinanceBlock extends Component{
         return(
             <div className='row d-flex align-items-center justify-content-center finance-block'>
                 <div className='col-sm'>
-                <img id='money'src={logo}/>
+                <img alt="" id='money'src={logo}/>
                 <p>From Start-up and Small business to Fortune 500 – we offer both in-house and partner finance and leasing programs to fit your specific needs</p>
                 <Link to='/finance'><button id='finance-button'>Financing Options</button></Link>
                 </div>
--- src/components/SaleSlider.jsx
+++ src/components/SaleSlider.jsx
@@ -1,6 +1,5 @@
 import React, {Component} from 'react'
 import {Carousel} from 'react-responsive-carousel'
-import styles from 'react-responsive-carousel/lib/styles/carousel.min.css'
 import fork0 from '../img/fork0.png'
 import fork1 from'../img/fork1.png'
 import fork2 from'../img/fork2.png'
@@ -23,14 +22,14 @@ class SaleSlide extends Component{
         };
             return(
                 <Carousel {...settings} >
-                   <img src={fork0}/>
-                   <img src={fork1}/>
-                   <img src={fork2}/>
-                   <img src={fork4}/>
-                   <img src={fork5}/>
-                   <img src={fork7}/>
-                   <img src={fork8}/>
-                   <img src={fork9}/>
+                   <img alt="" src={fork0}/>
+                   <img alt="" src={fork1}/>
+                   <img alt="" src={fork2}/>
+                   <img alt="" src={fork4}/>
+                   <img alt="" src={fork5}/>
+                   <img alt="" src={fork7}/>
+                   <img alt="" src={fork8}/>
+                   <img alt="" src={fork9}/>
                   
                 </Carousel>
         );
